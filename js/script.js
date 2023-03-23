895dbe7330d263d40fd0a25ba2e8c5a896aab9ff
Dina Polousova <dinapolousova@gmail.com>
2023-03-23 22:05:58 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+
+    //Tabs
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -34,4 +36,58 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
     });
+// Timer
+
+    let deadline = '2023-03-26';
+
+    //функция, определяющая остаток времени до дэдлайна
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()), //вычисляем количество миллисекунд от установленного времени окончания до текущего времени
+        seconds = Math.floor((t/1000) % 60), //вычисляем количество целых секунд
+        minutes = Math.floor((t/1000/60) % 60), //вычисляем количество целых минут
+        hours = Math.floor((t/(1000*60*60))); //вычисляем количество целых часов
+        //hours = Math.floor((t/1000/60/60) % 24), //если нужны также дни
+        //days = Math.floor((t/(1000*60*60*24)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+    
+    //функция для динамической вставки вычисленных значений времени в верстку
+    function setClock(id, endtime) { 
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+            //функция, обновляющая таймер 
+            function updateClock() {
+                let t = getTimeRemaining(endtime);
+
+                function addZero(num){
+                            if(num <= 9) {
+                                return '0' + num;
+                            } else return num;
+                        };
+    
+                hours.textContent = addZero(t.hours);
+                minutes.textContent = addZero(t.minutes);
+                seconds.textContent = addZero(t.seconds);
+    
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+                    hours.textContent = '00';
+                    minutes.textContent = '00';
+                    seconds.textContent = '00';
+                }
+            }
+    
+        }
+    
+        setClock('timer', deadline);
 });
