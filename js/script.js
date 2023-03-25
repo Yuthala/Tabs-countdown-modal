cafdf9a7de694be1aef2edd8647310f091cf1fb6
Dina Polousova <dinapolousova@gmail.com>
2023-03-25 19:19:40 +0300

    countdown

--- js/script.js
+++ js/script.js
@@ -92,4 +92,16 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     
         setClock('timer', deadline);
+
+
+//Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = "block";
+        this.classList.add('more-splash');
+    })
 });
