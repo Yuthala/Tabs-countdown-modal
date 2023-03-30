913a99bb3e9b11a442ce8262d730eb97f95b711a
Dina Polousova <dinapolousova@gmail.com>
2023-03-30 21:20:20 +0300

    form

--- js/script.js
+++ js/script.js
@@ -121,4 +121,47 @@ window.addEventListener('DOMContentLoaded', function() {
             document.body.style.overflow = 'hidden';
         })
     })
+
+    //Form
+
+    //создаем объект для сообщений
+    let message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    }
+
+    let form = document.querySelector('.main-form'), //форма из модального окна
+        input = form.getElementsByTagName('input'), //инпуты в этой форме
+        statusMessage =  document.createElement('div'); //создаем новый элемент, в который будем выводить сообщения для пользователя
+
+        statusMessage.classList.add('status');  //добавляем этому диву класс для стилизации
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault;
+        form.appendChild(statusMessage); //добавляем созданный <div> в форму, сюда будем выводить сообщение для пользователя
+
+        let request = new XMLHttpRequest();//создаем XMLHttp запрос 
+        request.open('POST', 'server.php');//настраиваем XMLHttp запрос 
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); //настраиваем заголовки  XMLHttp запроса 
+
+        let formData = new formData(form);//получить данные, введенные пользователем
+        request.send(formData);//отправить запрос на сервер, указав в body запроса объект formData
+
+        //выводим сообщение пользователю, успешно ли отправлена форма
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) { //если идет загрузка (статус запроса 0-3)
+                statusMessage.innerHTML = message.loading; //в <div> помещаем сообщение из объекта message по ключу loading  
+            } else if(request.readyState === 4 && request.status == 200) {  //если запрос успешно отправлен (статус запроса = 4 и сервер ответил кодом 200 (ОК))
+                statusMessage.innerHTML = message.success; //в <div> помещаем сообщение из объекта message по ключу success
+            } else {
+                statusMessage.innerHTML = message.failure; //в <div> помещаем сообщение из объекта message по ключу failure
+            }
+        });
+            //очищаем форму после отправки
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = ''; //присваиваем пустую строку в value каждому инпуту формы
+            }
+    });
+
 });
