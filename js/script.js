54268e525a2db90c21b063830598800875ce7fe9
Dina Polousova <dinapolousova@gmail.com>
2023-04-16 07:51:16 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -173,4 +173,63 @@ window.addEventListener('DOMContentLoaded', function() {
             }
     });
 
+//Slider
+
+    let slideIndex = 1, //Параметр текущего слайда
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    //функция, показывающая и скрывающая слайды и точки
+    function showSlides(n) {
+
+        if(n > slides.length) { //если мы дошли до последнего слайда
+            slideIndex = 1;
+        }
+        if (n < 1) { //если мы на первом слайде нажимаем стрелку "назад"
+            slideIndex = slides.length;
+        }
+
+
+        slides.forEach((item) => item.style.display = 'none');//скрываем все слайды
+        dots.forEach((item) => item.classList.remove('dot-active'));//скрываем точки
+
+        slides[slideIndex - 1].style.display = 'block';//показываем первый слайд (slideIndex = 0)
+        dots[slideIndex -1].classList.add('dot-active');//показываем первую точку
+    }
+
+    //функция, показывающая следующий слайд
+    function plusSlides (n) {
+        showSlides(slideIndex += n);
+    }
+    //функция, показывающая текущий слайд
+    function currentSlide (n) {
+        showSlides(slideIndex = n);
+    }
+
+    //показываем предыдущий слайд по клику "назад"
+    prev.addEventListener('click',  function() {
+        plusSlides(-1);
+    });
+
+    //показываем следующий слайд по клику "вперед"
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    //делегируем событие клик обертке точек; показываем слайд, который соответствует нажатой точке
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) { //i < dots.length + 1, потому что нам нужно сделать дополнительный проход цикла (если нажата четвертая точка, i = 3, и проход цикла закончится на предпоследнем слайде)
+
+        //проверяем, что пользователь кликнул именно на точку (event.target имеет класс .dot) и нажата соответствующая точка
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i); //показываем текущий слайд
+            }
+        }
+    });
+
 });
